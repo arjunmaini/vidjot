d1341ced786c9f5c70c90f4a325129dc500cc1b5
ARJUN MAINI <you@example.com>
2017-11-22 21:07:11 +0530

    initial commit

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-if(app.get('env') === production){
+if(process.env.NODE_ENV === 'production'){
     module.exports = {mongoURL : 'mongodb://<arjunmaini007>:<Arjun1997>@ds115396.mlab.com:15396/ajdb1'}
 }else{
     module.exports = {mongoURL : 'mongodb://localhost/vidjot-dev'}
